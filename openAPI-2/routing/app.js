bd69353763863e37f12071a5d19005364ecba667
OsborneLinus <linusholm1997@gmail.com>
2023-12-01 15:30:48 +0100

    Added fourSquare API to work with Google Maps API

--- openAPI-2/routing/app.js
+++ openAPI-2/routing/app.js
@@ -34,8 +34,14 @@ function drawRoute(routeData) {
 
 function getLocation() {
   navigator.geolocation.getCurrentPosition(
-    (position) => updateMapPosition(position),
-    (error) => console.log("THIS IS AN ERROR", err)
+    (position) => {
+      updateMapPosition(position);
+      placeSearch({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    },
+    (error) => console.log("THIS IS AN ERROR", error)
   );
 }
 
@@ -47,6 +53,7 @@ function updateMapPosition(position) {
   new google.maps.Marker({
     map: myMap,
     position: currentPosition,
+    icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
   });
 }
 
@@ -54,9 +61,71 @@ function renderMap() {
   myMap = new google.maps.Map(document.querySelector("#myMap"), {
     zoom: 15,
     center: {
-      lat: 57.8650895230837,
-      lng: 11.914371511068959,
+      lat: 57.86,
+      lng: 11.91,
     },
-    mapId: "ccb72d7b72697af",
+    mapId: "fb0a21bf1dd5baf4",
   });
 }
+
+async function placeSearch(currentPosition) {
+  try {
+    const formattedCoordinates = `${currentPosition.lat},${currentPosition.lng}`;
+    const searchParams = new URLSearchParams({
+      query: "coffee",
+      ll: formattedCoordinates,
+      open_now: "true",
+      sort: "DISTANCE",
+      v: "20231120",
+    });
+    const results = await fetch(
+      `https://api.foursquare.com/v3/places/search?${searchParams}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: "#=",
+        },
+      }
+    );
+    const data = await results.json();
+    data.results.forEach((result) => {
+      console.log(result);
+
+      if (
+        result.categories.some(
+          (category) =>
+            category.name === "Coffee Shop" || category.name === "Café"
+        )
+      ) {
+        var infoWindow = new google.maps.InfoWindow({
+          content:
+            "Name: " +
+            result.name +
+            "<br>Address: " +
+            result.location.address +
+            "<br>Open: " +
+            result.closed_bucket +
+            "<br>Distance: " +
+            result.distance +
+            "M",
+        });
+        if (result.location) {
+          new google.maps.Marker({
+            map: myMap,
+            position: {
+              lat: result.geocodes.main.latitude,
+              lng: result.geocodes.main.longitude,
+            },
+            title: result.name,
+          }).addListener("click", function () {
+            infoWindow.open(myMap, this);
+          });
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+placeSearch();
